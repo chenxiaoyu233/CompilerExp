4f9353a6e1a61f60690c41faea3499be3d306a0b
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2019-03-09 15:42:12 +0800

    [new feature] add the common utility header

---
+++ tools/common.h
@@ -0,0 +1,18 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+// STL LIB
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <bitmap>
+using namespace std;
+
+// Tools
+#include "RegExp.h"
+
+#endif
