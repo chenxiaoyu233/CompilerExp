e3b3327b422b0879472b64d7827fc4e9355e762c
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2019-03-10 21:19:21 +0800

    [new feature] 完善了词法规则并进行了大量测试

--- Lex/LexicalDefinition.cpp
+++ Lex/LexicalDefinition.cpp
@@ -1,33 +1,57 @@
 LexicalAnalyzer* LexicalDefinition() {
+	// setting the aux charactor sets
+	string az = "abcdefghijklmnopqrstuvwxyz";
+	string AZ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string digit = "0123456789";
+	string hex = "0123456789abcdeABCDE";
+	string alphabet = "";
+	// reverse "for" to eliminate the exists "\]" in the alphabet
+	for (int i = 127; i > 0; i--) if(char(i) != '\"' && char(i) != '\'') alphabet += char(i);
+	string commentCharSet = "";
+	for (int i = 127; i >= -128; i--) if(char(i) != '\n') commentCharSet += char(i);
+	
 	size_t keywordLevel = 10, constantLevel = 2, symbolLevel = 1;
 	LexicalAnalyzer *lex = new LexicalAnalyzer();
 	// white space
 	lex -> AddLexicalItem("\\[ \t\n\\]\\{1, \\}", "WHITE-SPACE", false);
 
+	// comment
+	lex -> AddLexicalItem("\\$//\\$ \\+ \\["+commentCharSet+"\\]\\{, \\}", "LINE-COMMENT", false);
+	lex -> AddLexicalItem("\\$/*\\$", "BLOCK-COMMENT-BEGIN", false);
+	lex -> AddLexicalItem("\\$*/\\$", "BLOCK-COMMENT-END", false);
+
 	// symbol
 	lex -> AddLexicalItem("\
-		\\[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\\] \\+ \
+		\\["+az+AZ+"_\\] \\+ \
 		\\(\
-			\\[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\\] \\|\
-			\\[0123456789\\] \\|\
+			\\["+az+AZ+"\\] \\|\
+			\\["+digit+"\\] \\|\
 			\\[_\\]\
 		\\)\\{, \\}", "SYMBOL", true, symbolLevel);
 
 	// constant
-	lex -> AddLexicalItem("\\[123456789\\] \\+ \\[0123456789\\]\\{, \\}", "INT-CONSTANT", true, constantLevel);
-	lex -> AddLexicalItem("\\[123456789\\] \\+ \\[0123456789\\]\\{, \\} \\+ \\[Ll\\]", "LONG-INT-CONSTANT", true, constantLevel);
-	lex -> AddLexicalItem("\\$0\\$ \\+ \\[xX\\] \\+ \\[0123456789abcdeABCDE\\]", "HEX-INT-CONSTANT", true, constantLevel);
-	lex -> AddLexicalItem("\\$0\\$ \\+ \\[xX\\] \\+ \\[0123456789abcdeABCDE\\] \\+ \\[Ll\\]", "HEX-LONG-INT-CONSTANT", true, constantLevel);
+	lex -> AddLexicalItem("\\[123456789\\] \\+ \\["+digit+"\\]\\{, \\} \\| \\$0\\$", "INT-CONSTANT", true, constantLevel);
 	lex -> AddLexicalItem("\
 		\\(\
-			\\[123456789\\] \\+ \\[0123456789\\]\\{, \\} \\|\
+			\\[123456789\\] \\+ \\["+digit+"\\]\\{, \\} \\| \\$0\\$\
+		\\) \\+ \\[Ll\\]", "LONG-INT-CONSTANT", true, constantLevel);
+	lex -> AddLexicalItem("\\$0\\$ \\+ \\[xX\\] \\+ \\["+hex+"\\]", "HEX-INT-CONSTANT", true, constantLevel);
+	lex -> AddLexicalItem("\\$0\\$ \\+ \\[xX\\] \\+ \\["+hex+"\\] \\+ \\[Ll\\]", "HEX-LONG-INT-CONSTANT", true, constantLevel);
+	lex -> AddLexicalItem("\
+		\\(\
+			\\[123456789\\] \\+ \\["+digit+"\\]\\{, \\} \\|\
 			\\$0\\$\
-		\\)\\{0, 1\\} \\+ \\$.\\$ \\+ \\[0123456789\\]\\{1, \\} \\+ \
-		\\(																\
-			\\$e\\$ \\+ \\[-\\]\\{0, 1\\} \\+ \\[123456789\\] \\+ \\[0123456789\\]\\{, \\}\
+		\\)\\{0, 1\\} \\+ \\$.\\$ \\+ \\["+digit+"\\]\\{1, \\} \\+ \
+		\\(\
+			\\$e\\$ \\+ \\[-\\]\\{0, 1\\} \\+ \\[123456789\\] \\+ \\["+digit+"\\]\\{, \\}\
 		\\)\\{0, 1\\}", "FLOAT-CONSTANT", true, constantLevel);
+	lex -> AddLexicalItem("\\$\"\\$ \\+ \\["+alphabet+"\\]\\{, \\} \\+ \\$\"\\$", "STRING-CONSTANT", true, constantLevel);
+	cerr << "\\$\"\\$ \\+ \\["+alphabet+"\\]\\{, \\} \\+ \\$\"\\$" << endl;
+	lex -> AddLexicalItem("\\$\'\\$ \\+ \\["+alphabet+"\\] \\+ \\$\'\\$", "CHAR-CONSTANT", true, constantLevel);
+	lex -> AddLexicalItem("\\$NULL\\$", "POINTER-CONSTANT", true, constantLevel);
 
 	// type ,class, declare
+	lex -> AddLexicalItem("\\$void\\$", "VOID-TYPE", true, keywordLevel);
 	lex -> AddLexicalItem("\\$int\\$", "INT-TYPE", true, keywordLevel);
 	lex -> AddLexicalItem("\\$float\\$", "FLOAT-TYPE", true, keywordLevel);
 	lex -> AddLexicalItem("\\$double\\$", "DOUBLE-TYPE", true, keywordLevel);
@@ -45,12 +69,20 @@ LexicalAnalyzer* LexicalDefinition() {
 	lex -> AddLexicalItem("\\$for\\$", "FOR", true, keywordLevel);
 	lex -> AddLexicalItem("\\$do\\$", "DO", true, keywordLevel);
 	lex -> AddLexicalItem("\\$goto\\$", "GOTO", true, keywordLevel);
+	lex -> AddLexicalItem("\\$switch\\$", "SWITCH", true, keywordLevel);
+	lex -> AddLexicalItem("\\$case\\$", "CASE", true, keywordLevel);
+	lex -> AddLexicalItem("\\$break\\$", "BREAK", true, keywordLevel);
+	lex -> AddLexicalItem("\\$continue\\$", "CONTINUE", true, keywordLevel);
+	lex -> AddLexicalItem("\\$return\\$", "RETURN", true, keywordLevel);
+	lex -> AddLexicalItem("\\$typedef\\$", "TYPEDEF", true, keywordLevel);
+	lex -> AddLexicalItem("\\$using\\$", "USING", true, keywordLevel);
 
 	// operator and expression
 	lex -> AddLexicalItem("\\$+\\$", "+", true);
 	lex -> AddLexicalItem("\\$-\\$", "-", true);
 	lex -> AddLexicalItem("\\$*\\$", "*", true);
 	lex -> AddLexicalItem("\\$/\\$", "/", true);
+	lex -> AddLexicalItem("\\$%\\$", "%", true);
 	lex -> AddLexicalItem("\\$<\\$", "<", true);
 	lex -> AddLexicalItem("\\$<=\\$", "<=", true);
 	lex -> AddLexicalItem("\\$>=\\$", ">=", true);
@@ -59,17 +91,22 @@ LexicalAnalyzer* LexicalDefinition() {
 	lex -> AddLexicalItem("\\$==\\$", "==", true);
 	lex -> AddLexicalItem("\\$=\\$", "=", true);
 	lex -> AddLexicalItem("\\$+=\\$", "+=", true);
+	lex -> AddLexicalItem("\\$++\\$", "++", true);
 	lex -> AddLexicalItem("\\$-=\\$", "-=", true);
+	lex -> AddLexicalItem("\\$--\\$", "--", true);
 	lex -> AddLexicalItem("\\$*=\\$", "*=", true);
 	lex -> AddLexicalItem("\\$/=\\$", "/=", true);
 	lex -> AddLexicalItem("\\$^=\\$", "^=", true);
 	lex -> AddLexicalItem("\\$&=\\$", "&=", true);
 	lex -> AddLexicalItem("\\$<<\\$", "<<", true);
 	lex -> AddLexicalItem("\\$>>\\$", ">>", true);
+	lex -> AddLexicalItem("\\$.\\$", ".", true);
+	lex -> AddLexicalItem("\\$->\\$", "->", true);
 	lex -> AddLexicalItem("\\$&\\$", "&", true);
 	lex -> AddLexicalItem("\\$|\\$", "|", true);
 	lex -> AddLexicalItem("\\$^\\$", "^", true);
 	lex -> AddLexicalItem("\\$?\\$", "?", true);
+	lex -> AddLexicalItem("\\$!\\$", "!", true);
 	lex -> AddLexicalItem("\\$(\\$", "(", true);
 	lex -> AddLexicalItem("\\$)\\$", ")", true);
 	lex -> AddLexicalItem("\\$[\\$", "[", true);
@@ -80,6 +117,8 @@ LexicalAnalyzer* LexicalDefinition() {
 	lex -> AddLexicalItem("\\$;\\$", ";", true);
 	lex -> AddLexicalItem("\\$:\\$", ":", true);
 	lex -> AddLexicalItem("\\$::\\$", "::", true);
+	lex -> AddLexicalItem("\\$&&\\$", "&&", true);
+	lex -> AddLexicalItem("\\$||\\$", "||", true);
 
 	return lex;
 }
