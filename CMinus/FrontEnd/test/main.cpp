67779c005fbbbc071645c380c2f3f4f6e3bd1d84
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2019-05-10 11:11:55 +0800

    [new feature] Prof.'s require

--- CMinus/FrontEnd/test/main.cpp
+++ CMinus/FrontEnd/test/main.cpp
@@ -53,14 +53,26 @@ void lex() {
     lexResult = lex -> Result();
     if (stop_at == "lex") {
         /* return the result from the lex stage, and halt */
-        /*symbol table to fool some fool*/
-        map<string, int> ST; 
-        int addr_cnt = 0;
-        for (auto item: lexResult) ST[item.content] = ++addr_cnt;
+        /*symbol table to*/
+        char buffer[10005];
+        char *pt = buffer;
+        unordered_map<string, char*> ST; 
+        for (auto item: lexResult) {
+            //ST[item.content] = strdup(item.content.c_str());
+            ST[item.content] = pt;
+            for (auto c: item.content) *(pt++) = c;
+            *(pt++) = '\0';
+        }
         printf("%10s %10s %10s %10s %10s\n", "content", "sym-type", "begin", "end", "addr");
         printf("%10s %10s %10s %10s %10s\n", "-------", "--------", "-----", "---", "----");
         for (auto item: lexResult) {
-            printf("%10s %10s %10zu %10zu %10d\n", item.content.c_str(), item.symbolType.c_str(), item.begin, item.end, ST[item.content]);
+            printf("%10s %10s %10zu %10zu %10p\n", item.content.c_str(), item.symbolType.c_str(), item.begin, item.end, ST[item.content]);
+        }
+        printf("Symbol Table\n");
+        printf("%10s %20s %10s\n", "symbol", "addr", "content");
+        printf("%10s %20s %10s\n", "------", "----", "-------");
+        for (auto pr: ST) {
+            printf("%10s %20p %10s\n", pr.first.c_str(), pr.second, pr.second);
         }
         exit(0);
     }
--- tools/common.hpp
+++ tools/common.hpp
@@ -8,6 +8,7 @@
 #include <cstdio>
 #include <string>
 #include <map>
+#include <unordered_map>
 #include <set>
 #include <queue>
 #include <bitset>
